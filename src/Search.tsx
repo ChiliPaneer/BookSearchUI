1f4eb65abec9c9f3f221745b277fd62b44f062dc
chiliPaneer <venkatratnamcp@gmail.com>
2024-05-13 01:27:22 -0400

    added bootstrap and search bar

--- src/Search.tsx
+++ src/Search.tsx
@@ -1,7 +1,18 @@
 import React from 'react';
+import Form from 'react-bootstrap/Form';
 
 export default function Search() {
   return (
-    <p>Future search box</p>
+    <>
+      <Form.Label htmlFor="bookSearch">Book Search</Form.Label>
+      <Form.Control
+        type="text"
+        id="book_query"
+        aria-describedby="bookSearchBar"
+      />
+      <Form.Text id="bookSearchQuery" muted>
+      Type your book query above
+      </Form.Text>
+    </>
   )
 }
